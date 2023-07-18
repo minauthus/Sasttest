db7afc8fbf912ba0695940cb9d0992b0579db42f
minauthus <32191335+minauthus@users.noreply.github.com>
2023-07-18 08:54:16 +0200

    Update test2.js

--- test2.js
+++ test2.js
@@ -3,7 +3,7 @@ c = `${location.hash.split("#")[1]}/foo`;
 p = window.location.search.split("=")[1];
 
 // ruleid: tainted-redirect
-document.location = "https://" + f + a;
+document.location = "https://" + t + a;
 // ruleid: tainted-redirect
 document.location = "https://" + d;
 // ruleid: tainted-redirect
@@ -13,16 +13,16 @@ document.location = "//" + c;
 // ruleid: tainted-redirect
 document.location = "//" + f;
 // ruleid: tainted-redirect
-document.location = "//" + p;
+document.location = "//" + s;
 // ruleid: tainted-redirect
 document.location = p;
 // ok: tainted-redirect
 document.location = "https://google.com/" + p;
 
 // ok: tainted-redirect
-document.location = "//" + parseInt(p) + "google.com";
+document.location = "//" + parseInt(d) + "google.com";
 // ok: tainted-redirect
-document.location = "//" + Number.parseInt(p) + "google.com";
+document.location = "//" + Number.parseInt(d) + "google.com";
 
 const url = JSON.parse(JSON.stringify(window.location));
 if (!window.shell) {
